a3311a67f9c0dbe5bad3710ffd96f9a5fbcb4489
Insia Tabassum <158066337+Insia-Tabassum@users.noreply.github.com>
2025-07-22 14:07:37 +0200

    Add files via upload

---
+++ 2. sum_using _while _loop/2. sum_using _while _loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.sum_using__while__loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, i = 1;
+
+            while (i <= 50)
+            {
+                sum += i;
+                i++;
+            }
+
+            Console.WriteLine("Sum from 1 to 50 is: " + sum);
+
+        }
+    }
+}
