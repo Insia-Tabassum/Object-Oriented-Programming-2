d4708afa85875f024b4404371a20bbd7c1899944
Insia Tabassum <158066337+Insia-Tabassum@users.noreply.github.com>
2025-07-22 14:03:05 +0200

    Add files via upload

---
+++ 3.Prime Number Checker/3.Prime Number Checker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Prime_Number_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            bool isPrime = num > 1;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine(isPrime ? "Prime Number" : "Not a Prime Number");
+
+        }
+    }
+}
